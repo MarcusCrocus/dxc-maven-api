8c9f4ca5b5ff36b45f50b421b910fa4db043f9b0
Marcus <crocushtml@gmail.com>
2024-03-14 11:10:18 +0100

    starting to writh controller and testing with postman

--- src/main/java/dxcapimaven/api/ApiApiController.java
+++ src/main/java/dxcapimaven/api/ApiApiController.java
@@ -1,10 +1,17 @@
 package dxcapimaven.api;
 
 
+import dxcapimaven.model.Client;
+import dxcapimaven.model.GetApiClients201Response;
+import dxcapimaven.model.PostApiClients201Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import javax.annotation.Generated;
 
@@ -13,15 +20,38 @@ import javax.annotation.Generated;
 @RequestMapping("${openapi.apiTest.base-path:}")
 public class ApiApiController implements ApiApi {
 
-    private final ApiApiDelegate delegate;
 
-    public ApiApiController(@Autowired(required = false) ApiApiDelegate delegate) {
-        this.delegate = Optional.ofNullable(delegate).orElse(new ApiApiDelegate() {});
+    //    private final ApiApiDelegate delegate;
+    //
+    //    public ApiApiController(@Autowired(required = false) ApiApiDelegate delegate) {
+    //        this.delegate = Optional.ofNullable(delegate).orElse(new ApiApiDelegate() {});
+    //    }
+    //
+    //    @Override
+    //    public ApiApiDelegate getDelegate() {
+    //        return delegate;
+    //    }
+    List<Client> clientList = new ArrayList<>();
+    @Override
+    public ResponseEntity<PostApiClients201Response> postApiClients(List<Client> client) {
+
+        clientList.addAll(client);
+
+        PostApiClients201Response response = new PostApiClients201Response();
+        response.setData(clientList);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+
+        //        return ApiApi.super.postApiClients(clients);
+
     }
 
     @Override
-    public ApiApiDelegate getDelegate() {
-        return delegate;
+    public ResponseEntity<GetApiClients201Response> getApiClients(Integer limitParam, Integer offsetParam, Integer sortBy) {
+
+        GetApiClients201Response response = new GetApiClients201Response();
+        response.setData(clientList);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
