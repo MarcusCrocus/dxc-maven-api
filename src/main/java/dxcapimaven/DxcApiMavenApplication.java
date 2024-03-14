f5d4ca0d56c39c59caf4439ee58d78974391e8d1
Marcus <crocushtml@gmail.com>
2024-03-14 09:38:40 +0100

    clean projetc form spring initializr - START

---
+++ src/main/java/dxcapimaven/DxcApiMavenApplication.java
@@ -0,0 +1,13 @@
+package dxcapimaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxcApiMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DxcApiMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dxcapimaven/DxcApiMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package dxcapimaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxcApiMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
